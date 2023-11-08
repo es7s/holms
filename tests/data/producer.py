c778345f78e4368d659de30920831a390a27e97a
delameter <0.delameter@gmail.com>
2023-11-08 04:40:09 +0300

    🌱 NEW: `raw`, `type`, `typename` columns 🌱 NEW: `--legend` option 💎 REFACTOR: basically the whole `writer` module
    📙 DOCS: README.md

--- tests/data/producer.py
+++ tests/data/producer.py
@@ -2,7 +2,6 @@
 #  es7s/holms
 #  (c) 2023 A. Shavykin <0.delameter@gmail.com>
 # ------------------------------------------------------------------------------
-
 import random
 import time
 
@@ -11,8 +10,8 @@ while True:
         n = random.randint(0, 0x100000)
         r = [1, random.randint(1,5)][random.randint(0, 100) > 90]
         print(chr(n)*r, end='', flush=True)
-        if n < 0x80:
-            break
+        # if n < 0x80:
+        #     break
     except:
         print(random.randbytes(6), end='', flush=True)
     time.sleep(0.3)
